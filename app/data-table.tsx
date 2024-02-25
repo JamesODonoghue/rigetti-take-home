464f481277d93e12f3d62ff4b8648d8e31aac393
James O'Donoghue <jameso@astra.com>
2024-02-24 23:16:35 -0800

    chore: remove date picker

--- app/data-table.tsx
+++ app/data-table.tsx
@@ -29,18 +29,6 @@ import {
   SelectLabel,
   SelectItem,
 } from "@/components/ui/select";
-import { cn } from "@/lib/utils";
-
-import { addDays, format } from "date-fns";
-import { CalendarIcon } from "lucide-react";
-import { DateRange } from "react-day-picker";
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -53,11 +41,6 @@ export function DataTable<TData, TValue>({
 }: DataTableProps<TData, TValue>) {
   const [sorting, setSorting] = useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
-  const [date, setDate] = useState<DateRange | undefined>({
-    from: new Date(2016, 0, 20),
-    to: addDays(new Date(2016, 0, 20), 20),
-  });
-
   const table = useReactTable({
     data,
     columns,
@@ -93,42 +76,6 @@ export function DataTable<TData, TValue>({
           </SelectGroup>
         </SelectContent>
       </Select>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            id="date"
-            variant={"outline"}
-            className={cn(
-              "w-[300px] justify-start text-left font-normal",
-              !date && "text-muted-foreground"
-            )}
-          >
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            {date?.from ? (
-              date.to ? (
-                <>
-                  {format(date.from, "LLL dd, y")} -{" "}
-                  {format(date.to, "LLL dd, y")}
-                </>
-              ) : (
-                format(date.from, "LLL dd, y")
-              )
-            ) : (
-              <span>Pick a date</span>
-            )}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0" align="start">
-          <Calendar
-            initialFocus
-            mode="range"
-            defaultMonth={date?.from}
-            selected={date}
-            onSelect={setDate}
-            numberOfMonths={2}
-          />
-        </PopoverContent>
-      </Popover>
       <div className="rounded-md border">
         <Table>
           <TableHeader>
